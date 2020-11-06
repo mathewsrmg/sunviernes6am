32371079816955dba5133a329b71c3ae8d3135fc
Mathew <mathewsrmg@gmail.com>
2020-11-06 08:35:23 -0500

    mucho voltaje pa mi cerebro
    la conexion a la base de datos cambios en el registro y como ver con imprimir el arreglo de los datos que estan ahi y ya

--- Basedatos.php
+++ Basedatos.php
@@ -9,18 +9,53 @@ class Basedatos{
     //CONSTRUCTOR
     public function __construct(){}
 
-    //FUNCIONES O METODOS
+    //FUNCIONES O METODOS este es para conectarnos en la base de datos 
     public function conectarBD(){
   
         try{
             $datosBD="mysql:host=localhost;dbname=tienda6";
             $conexionBD=new PDO($datosBD,$this->usuarioBD,$this->passwordBD);
-            echo("conexion exitosa");
+            return($conexionBD);
+
         }catch(PDOExeption $error){
 
             echo($error->getMessage());
             
         }
+    }
+
+    public function agregarDatos($consultaSQL){
+        //1. se debe establecer una conexion con la BD
+        $conexionBD=$this->conectarBD();
+
+        //2.preparar la consulta para insertar datos
+        $consultaAgregarDatos=$conexionBD->prepare($consultaSQL);
+
+        //3.ejecutar la consulta
+        $resultado=$consultaAgregarDatos->execute();
+
+        //4.validar la operacion
+        if($resultado){
+            echo("exito insertando los datos");
+        }else{
+            echo("error insertando los datos");
+        }
+    }
+    public function consultarDatos($consultaSQL){
+        //1. se debe establecer una conexion con la BD
+        $conexionBD=$this->conectarBD();
+
+        //2.preparar la consulta para insertar datos
+        $consultaBuscarDatos=$conexionBD->prepare($consultaSQL);
+
+        //3.indicar el metodo para traer los datos
+        $consultaBuscarDatos->setFetchMode(PDO::FETCH_ASSOC);
+
+        //4.se ejecuta la consulta
+        $resultado=$consultaBuscarDatos->execute();
+
+        //5.retorne los datos consultados!
+        return($consultaBuscarDatos->fetchAll());
     }
 }
 
--- registrarUsuarios.php
+++ registrarUsuarios.php
@@ -3,18 +3,21 @@
     include("Basedatos.php");
 
     if (isset($_POST["botonEnvio"])){
-    //recibir los datos
+    //1.recibir los datos
     $nombre=$_POST["nombre"];
     $apellido=$_POST["apellido"];
     $descripcion=$_POST["descripcion"];
     $genero=$_POST["genero"];
-
-    //crear una copia de la BD
-    //crear un ubjeto de la clase BD
+    }
+    
+    //2.crear una copia de la BD 
+    //crear un ubjeto de la clase BD acceder al metodo
     $transaccion=new Basedatos();
 
-    $transaccion->conectarBD();
-
-    }
-
+    //3.crear una consulta SQL para agregar datos
+    $consultaSQL="INSERT INTO usuarios(nombre, apellido, descripcion, genero) VALUES ('$nombre','$apellido','$descripcion','$genero')";
+    
+    //4.utilizar el metodo agregarDatos()
+    $transaccion->agregarDatos($consultaSQL);
+    
 ?>
